5bbdbbce09af667bc273646013feea42b69367dd
HightowerPL <jakubsnopkiewicz@gmail.com>
2016-12-13 15:15:17 +0100

    update on landscape orientation screens

--- app/js/app.js
+++ app/js/app.js
@@ -6,37 +6,92 @@ $(document).ready(function(){
       $(window).scroll( function(){
           var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          $('.fade-in-block').each( function(i){
+          //Animations for Portrait Orientation
+          if ($(window).width() < $(window).height()) {
+            $('.fade-in-block').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1'},800);
+                }
+            });
+
+            $('.fade-in-top').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1', "top":'0'},800);
+                }
+            });
+
+            $('.fade-in-left').each( function(i){
               var bottom_of_object = $(this).offset().top + $(this).outerHeight();
 
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'opacity':'1'},800);
-              }
-          });
+                if( bottom_of_window > bottom_of_object ){
+                  $(this).animate({'opacity':'1', 'left' : '0'},800);
 
-          $('.fade-in-top').each( function(i){
-              var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                }
+            });
 
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'opacity':'1', "top":'0'},800);
-              }
-          });
+            $('.tele-pic04').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
 
-          $('.tele-pic04').each( function(i){
-              var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1', 'right': '0'},800);
+                }
+            });
 
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'opacity':'1', 'right': '0'},800);
-              }
-          });
+            $('.slide-in-bottom').each( function(i){
+                var bottom_of_object = $(this).offset().top;
 
-          $('.slide-in-bottom').each( function(i){
-              var bottom_of_object = $(this).offset().top;
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'bottom':'-2%', 'opacity':'1'},800);
+                }
+            });
+          }
+          //Animations for Landscape orientation
+          else{
+            $('.fade-in-block').each( function(i){
+                var middle_of_object = $(this).offset().top + $(this).outerHeight()*0.60;
 
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'bottom':'-2%', 'opacity':'1'},800);
-              }
-          });
+                if( bottom_of_window > middle_of_object ){
+                    $(this).animate({'opacity':'1'},800);
+                }
+            });
+
+            $('.fade-in-top').each( function(i){
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1', "top":'0'},800);
+                }
+            });
+
+            $('.fade-in-left').each( function(i){
+              var middle_of_object = $(this).offset().top + $(this).outerHeight()*0.35;
+
+                if( bottom_of_window > middle_of_object ){
+                    $(this).animate({'opacity':'1', 'left' : '0'},800);
+
+                }
+            });
+
+            $('.tele-pic04').each( function(i){
+              var middle_of_object = $(this).offset().top + $(this).outerHeight()*0.30;
+
+                if( bottom_of_window > middle_of_object ){
+                    $(this).animate({'opacity':'1', 'right': '0'},800);
+                }
+            });
+
+            $('.slide-in-bottom').each( function(i){
+                var top_of_object = $(this).offset().top - $(this).outerHeight()*0.5;
+
+                if( bottom_of_window > top_of_object ){
+                    $(this).animate({'bottom':'-2%', 'opacity':'1'},800);
+                }
+            });
+          }
 
       });
   });
